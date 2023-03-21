9b96c509ceb4aaf82d7b74a07cf2874d70c9acc2
secabstraction <jesse@secabstraction.com>
2023-03-21 09:41:14 -0700

    update probe offset

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1488,7 +1488,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -41, -11, -2.5 } // MicroSwiss NG
+#define NOZZLE_TO_PROBE_OFFSET { -41, -11, -2.7 } // MicroSwiss NG
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
