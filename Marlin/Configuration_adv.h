e36969ea3be9b91df6c90e39bd747f6b7ce5837e
secabstraction <jesse@secabstraction.com>
2023-03-24 16:02:06 -0700

    update z-axis current

--- Marlin/Configuration_adv.h
+++ Marlin/Configuration_adv.h
@@ -2757,7 +2757,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Z)
-    #define Z_CURRENT        700
+    #define Z_CURRENT        625
     #define Z_CURRENT_HOME   Z_CURRENT
     #define Z_MICROSTEPS     16
     #define Z_RSENSE         0.11
@@ -2767,7 +2767,7 @@
   #endif
 
   #if AXIS_IS_TMC_CONFIG(Z2)
-    #define Z2_CURRENT        700
+    #define Z2_CURRENT        625
     #define Z2_CURRENT_HOME   Z2_CURRENT
     #define Z2_MICROSTEPS     16
     #define Z2_RSENSE         0.11
